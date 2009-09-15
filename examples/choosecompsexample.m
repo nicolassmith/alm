9cda93a4513ab52a2604972b4b488376fd61c57e
Nicolas Smith <nicolas.d.smith@gmail.com>
2009-09-15 12:28:36 -0700

    fleshed out choose components example script

--- examples/choosecompsexample.m
+++ examples/choosecompsexample.m
@@ -2,6 +2,9 @@
 close all
 clear classes
 
+disp(' chooseComponents example script.')
+disp(' ')
+
 focalLengthList = [-.75;.5;1.75;-2;-1;2;3;1;2.5;1.25];
 lensList = component.lens(focalLengthList);
 
@@ -21,13 +24,13 @@ zdomain = -1:.01:6;
 
 figure(2)
 hold on
-goo.plotBeamWidth(zdomain)
+orighandle=goo.plotBeamWidth(zdomain);
 goo.plotComponents(zdomain)
 
 [pathList,overlapList] = goo.chooseComponents(...
                 'lens1',lensList,[0.5 3],...  % choose lens1 from the list,
                 'lens2',lensList.duplicate,[3.5 4],... %duplicate the list, this allows
-                ...                                    %the same component to be chosen more than once
+                ...                                    %  the same component to be chosen more than once
                 'target',[4.5,6]... % we can also allow the target waist position to vary while optimizing the overlap
                 ,'-vt',.25); % set the minimum initial overlap to 0.25, if a combination of components
                              % has an overlap less than this, it will be skipped without trying to optimize the lens positions
@@ -39,9 +42,31 @@ goo.plotComponents(zdomain)
 % then duplicate the list, which makes an array of new component objects which
 % are not linked to the originals.
 
-pathList(1).plotBeamWidth(zdomain,'r')
+% now let's cut out all solutions with modematching < 99%
+
+pathList = pathList(overlapList >= 0.99);
+
+% make an array with the combined position sensitivity of the components
+
+sensitivityList = pathList.positionSensitivity;
+
+% now sort in increasing sensitivity
+
+[sensitivityList,sortIndex] = sort(sensitivityList);
+pathList = pathList(sortIndex);
+
+% plot the best solution
+newhandle=pathList(1).plotBeamWidth(zdomain,'r');
 pathList(1).plotComponents(zdomain,'r*')
 pathList(1).plotBeams(zdomain,'k')
 
 hold off
+legend([orighandle newhandle],'Original Beam Path','Optimized Beam Path')
+ylabel('Beam Width (m)')
+xlabel('Propagation axis (m)')
 
+
+% print the component list to the command window
+disp(' ')
+disp(' Optimized Path Component List:')
+display(pathList(1).components)
