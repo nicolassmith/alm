7621096a0fb5e59b8efb8626abf837f2142b631f
Nicolas Smith <nicolas.d.smith@gmail.com>
2013-01-24 15:43:31 -0800

    makezip is slightly better, usage comments, make 'alm' string a variable.

--- makezip.sh
+++ makezip.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# makes zip archive for distribution, first argument is version number
+#
+# example: to make alm1.0.zip:
+# ./makezip.sh 1.0
 
+packagename="alm"
 pwd=`pwd`
 thisdir=`basename $pwd`
 
 cd ..
 
-find $thisdir -not -name "makezip.sh" -not -wholename "*/.git*"|zip ${thisdir}/alm${1}.zip -@
+find $thisdir -not -name "makezip.sh" -not -wholename "*/.git*" -not -name "${packagename}*.zip" |zip ${thisdir}/${packagename}${1}.zip -@
 
